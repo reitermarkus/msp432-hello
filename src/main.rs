182dfa5a9d5738019f6b64c53bb140411a8f56c5
Markus Reiter <me@reitermark.us>
2021-03-14 21:08:09 +0100

    Set clock speed.

--- src/main.rs
+++ src/main.rs
@@ -5,8 +5,9 @@ use cortex_m_rt::entry;
 use cortex_m::prelude::*;
 use cortex_m_semihosting::hprintln;
 
+use msp432p401r::CS;
 use msp432p401r_hal::watchdog::{WatchdogTimer, Enabled, Disable};
-use msp432p401r_hal::{clock::{CsExt, DcoclkFreqSel, DIVM_A, DIVS_A}, gpio::{GpioExt, InputPin, OutputPin, ToggleableOutputPin}};
+use msp432p401r_hal::{clock::{CsExt, DcoclkFreqSel, DIVM_A, DIVS_A}, gpio::{GpioExt, InputPin, OutputPin, ToggleableOutputPin}, pcm::{PcmConfig, PcmDefined, VCoreSel}};
 
 use panic_abort as _;
 
@@ -16,22 +17,30 @@ fn main() -> ! {
   let watchdog = WatchdogTimer::<Enabled>::new();
   watchdog.try_disable().unwrap();
 
-  let peripherals = cortex_m::Peripherals::take().unwrap();
-  let ahb_frequency = 3_000_000;
-  let mut timer = cortex_m::delay::Delay::new(peripherals.SYST, ahb_frequency);
+  let mut pcm: PcmConfig::<PcmDefined> = PcmConfig::<PcmDefined>::new();   // Setup PcmConfig
+          pcm.set_vcore(VCoreSel::DcdcVcore1);                             // Set DCDC Vcore1 -> 48 MHz Clock
+  let _pcm_sel = pcm.get_powermode();                                      // Get the current powermode
+
+  // hprintln!("Power Mode: {:?}", pcm_sel).unwrap();
+
 
   let p = msp432p401r::Peripherals::take().unwrap();
 
   // The Digital I/O module
   let dio = p.DIO.split();
 
-  let cs = p.CS.constrain();
+  let freq_sel = DcoclkFreqSel::_24MHz;
 
-  let _clocks = cs.mclk_dcoclk( DcoclkFreqSel::_48MHz, DIVM_A::DIVM_0)
-    .smclk_div(DIVS_A::DIVS_1)
-    .freeze();
+  let _clocks = p.CS.constrain()
+      .mclk_dcoclk(freq_sel, DIVM_A::DIVM_0)
+      .smclk_div(DIVS_A::DIVS_1)
+      .freeze();
 
+  let peripherals = cortex_m::Peripherals::take().unwrap();
+  let ahb_frequency = freq_sel.freq();
+  let mut timer = cortex_m::delay::Delay::new(peripherals.SYST, ahb_frequency);
 
+  #[cfg(debug)]
   hprintln!("Started.").unwrap();
 
   // PORTA consists of two ports P1 and P2. Red LED of the
@@ -40,29 +49,44 @@ fn main() -> ! {
   // to 1 and write a 1 to the output register to put ON the LED.
 
   let mut led1 = dio.p1_0.into_output();
-  led1.try_set_low().unwrap();
+  led1.try_set_high().unwrap();
 
   let mut rgbled_red = dio.p2_0.into_output();
-  rgbled_red.try_set_high().unwrap();
+  rgbled_red.try_set_low().unwrap();
   let mut rgbled_green = dio.p2_1.into_output();
-  rgbled_green.try_set_high().unwrap();
+  rgbled_green.try_set_low().unwrap();
   let mut rgbled_blue = dio.p2_2.into_output();
-  rgbled_blue.try_set_high().unwrap();
+  rgbled_blue.try_set_low().unwrap();
 
   let button1 = dio.p1_4.into_pull_up_input();
   let button2 = dio.p1_1.into_pull_up_input();
 
+  let mut color = 0;
+
   loop {
-    hprintln!("Loop.").unwrap();
+    // hprintln!("Loop.").unwrap();
 
     led1.try_toggle().unwrap();
-    rgbled_red.try_toggle().unwrap();
-    rgbled_green.try_toggle().unwrap();
-    rgbled_blue.try_toggle().unwrap();
 
-    hprintln!("Button 1: {}", button1.try_is_low().unwrap()).unwrap();
-    hprintln!("Button 2: {}", button2.try_is_low().unwrap()).unwrap();
+    match color {
+      0..=1 => {
+        rgbled_red.try_toggle().unwrap();
+      }
+      2..=3 => {
+        rgbled_green.try_toggle().unwrap();
+      }
+      4..=5 => {
+        rgbled_blue.try_toggle().unwrap();
+      }
+      _ => unreachable!() ,
+    }
 
-    timer.delay_ms(1000);
+    color = (color + 1) % 6;
+
+
+    // hprintln!("Button 1: {}", button1.try_is_low().unwrap()).unwrap();
+    // hprintln!("Button 2: {}", button2.try_is_low().unwrap()).unwrap();
+
+    timer.delay_ms(100);
   }
 }
